0070f9eb74a747d499497d36ecdb58ced0a7fe5c
ElizabethAS <elizabethantunes95@gmail.com>
2019-08-15 14:11:57 -0300

    Pagina de resultado da pesquisa

---
+++ result.php
@@ -0,0 +1,49 @@
+<?php 
+	include "cabecalho.php"
+?>
+<br>
+	<div class="conteiner1">
+		<section class="resultCont">
+			<div class="card2"><h3>Resultado:</h3></div>
+				<form class="form-group">
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h4>O IP inserido foi:</h4></label>
+						<br>
+						<label for="exampleFormControlInput1"><h4>A máscara inserida foi:</h4></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>A quantidade de sub-redes:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>O endereço de rede:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>O endereço do broadcast:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>O primeiro endereço de host:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>O último endereço de host:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>Nova máscara de rede em formato decimal:</h5></label>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>Classe do IP:</h5></label>
+						<h3><samp class="badge badge-pill badge-danger">A</samp></h3>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlInput1"><h5>O endereço de IP é:</h5></label>
+						<h3><samp class="badge badge-pill badge-danger">Privado</samp></h3>
+					</div>
+					<div>
+						<button type="submit" class="btn" id="btn-menu" action="">⬅ Menu</button>
+					</div>
+				</form>
+		</section>
+		
+	</div>
+<?php 
+	include "rodape.php"
+?>
